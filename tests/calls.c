86c1cadd7d62e4d4c759c94941a41172044187b1
Denis Oliver Kropp <dok@cyto.(none)>
2008-07-19 18:13:20 +0200

    [tests] Change calls test to use asynchronous (FCEF_ONEWAY) calls, more output.

--- tests/calls.c
+++ tests/calls.c
@@ -29,11 +29,15 @@
 
 #include <pthread.h>
 
+#include <direct/direct.h>
+#include <direct/messages.h>
 
-static int        fd;       /* File descriptor of the Fusion Kernel Device */
-static pthread_t  receiver; /* Thread reading messages from the device. */
 
-static int        counter = 0;
+static int          fd;       /* File descriptor of the Fusion Kernel Device */
+static pthread_t    receiver; /* Thread reading messages from the device. */
+
+static int          counter    = 0;
+static volatile int last_count = 0;
 
 /*
  * A simple example call handler
@@ -51,6 +55,14 @@ call_handler (int   caller,   /* fusion id of the caller */
 {
   counter++;
 
+  if (counter == 1)
+       D_INFO( "FusionTest/Calls: Processed first call...\n" );
+
+  if (counter == last_count)
+       D_INFO( "FusionTest/Calls: Processed last (%dth) call...\n", last_count );
+  else if (counter % 1000 == 0)
+       D_INFO( "FusionTest/Calls: Processed %dth call...\n", counter );
+
   return 0;
 }
 
@@ -64,6 +76,7 @@ process_call_message (int call_id, FusionCallMessage *msg)
   CallHandler      handler = msg->handler;
 
   call_ret.call_id = call_id;
+  call_ret.serial  = msg->serial;
   call_ret.val     = handler (msg->caller,
                               msg->call_arg,
                               msg->call_ptr,
@@ -81,6 +94,8 @@ receiver_thread (void *arg)
 {
   int  len;
   char buf[1024];
+
+  D_INFO( "FusionTest/Calls: Running dispatcher loop...\n" );
 
   /* Read as many messages as possible at once. */
   while ((len = read (fd, buf, 1024)) > 0 || errno == EINTR)
@@ -130,20 +145,26 @@ receiver_thread (void *arg)
 int
 main (int argc, char *argv[])
 {
-  long              d = 0;
+  long              c = 0, d = 0;
   FusionCallNew     call_new;
   FusionCallExecute call_exec;
   struct timeval    t1, t2;
 
   FusionEnter enter = {{ FUSION_API_MAJOR, FUSION_API_MINOR }};
 
+  direct_initialize();
+
+  D_INFO( "FusionTest/Calls: Opening Fusion Device...\n" );
+
   /* Open the Fusion Kernel Device. */
-  fd = open ("/dev/fusion/0", O_RDWR);
+  fd = open ("/dev/fusion0", O_RDWR | O_EXCL);
   if (fd < 0)
     {
       perror ("opening /dev/fusion failed");
       return -1;
     }
+
+  D_INFO( "FusionTest/Calls: Entering Fusion World...\n" );
 
   /* Query our fusion id. */
   if (ioctl (fd, FUSION_ENTER, &enter))
@@ -152,6 +173,8 @@ main (int argc, char *argv[])
       close (fd);
       return -2;
     }
+
+  D_INFO( "FusionTest/Calls: Entered as Fusionee with ID %lu.\n", enter.fusion_id );
 
   /* Install the call handler. */
   call_new.handler = call_handler;
@@ -164,45 +187,60 @@ main (int argc, char *argv[])
       return -3;
     }
 
+  D_INFO( "FusionTest/Calls: Created Call with ID %d.\n", call_new.call_id );
+
   /* Start the receiver thread. */
+  D_INFO( "FusionTest/Calls: Starting thread...\n" );
   pthread_create (&receiver, NULL, receiver_thread, NULL);
 
   /* Flush disk buffers and wait for the receiver being up. */
+  D_INFO( "FusionTest/Calls: Syncing disks...\n" );
   sync();
   usleep (100000);
 
   /* Specify call id to execute. */
   call_exec.call_id = call_new.call_id;
+  call_exec.flags   = FCEF_ONEWAY;
 
   /* Stop time before call loop. */
   gettimeofday (&t1, NULL);
 
+  D_INFO( "FusionTest/Calls: Running calling loop...\n" );
+
   /* Do as many calls as possible (in 3 seconds). */
-  while (d < 3000)
+  while (d < 3000000)
     {
       if (ioctl (fd, FUSION_CALL_EXECUTE, &call_exec))
         perror ("FUSION_CALL_EXECUTE");
 
       /* Stop time every 1000 calls. */
-      if (counter % 1000 == 0)
+      if (++c % 1000 == 0)
         {
           gettimeofday (&t2, NULL);
 
-          d = (t2.tv_sec - t1.tv_sec) * 1000 + (t2.tv_usec - t1.tv_usec) / 1000;
+          d = (t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_usec - t1.tv_usec);
         }
     }
 
-  /* Print message. */
-  printf ("Executed %ld synchronous RPC calls per second.\n",
-          counter * 1000 / d);
+  last_count = c;
 
-  /* Stop the receiver. */
-  pthread_cancel (receiver);
-  pthread_join (receiver, NULL);
+  D_INFO( "FusionTest/Calls: Destroying call...\n" );
 
   /* Destroy the call. */
   if (ioctl (fd, FUSION_CALL_DESTROY, &call_new.call_id))
     perror ("FUSION_CALL_DESTROY");
+
+  gettimeofday (&t2, NULL);
+
+  D_INFO( "FusionTest/Calls: Destroyed call.\n" );
+
+  /* Print message. */
+  D_INFO ("FusionTest/Calls: Executed %llu asynchronous RPC calls per second.\n",
+          counter * 1000000ULL / ((t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_usec - t1.tv_usec)));
+
+  /* Stop the receiver. */
+  pthread_cancel (receiver);
+  pthread_join (receiver, NULL);
 
   /* Close the Fusion Kernel Device. */
   close (fd);
