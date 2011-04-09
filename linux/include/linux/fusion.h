4bd0761bec5c4623fe2dc54ff3c4d1ef557596b8
Andreas Shimokawa <andi@directfb.org>
2011-04-09 18:24:50 +0200

    release 8.3.1

--- linux/include/linux/fusion.h
+++ linux/include/linux/fusion.h
@@ -20,7 +20,7 @@
 /* Fusion supports all API versions up to this version */
 #define FUSION_API_MAJOR_PROVIDED 8
 #define FUSION_API_MINOR_PROVIDED 3
-#define FUSION_API_MICRO_PROVIDED 0
+#define FUSION_API_MICRO_PROVIDED 1
 
 /*
  * Fusion Kernel Device API Version
