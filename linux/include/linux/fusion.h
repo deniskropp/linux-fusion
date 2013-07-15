a46139ef2570de106799b235203eb22185239180
Andreas Shimokawa <andi@directfb.org>
2013-07-15 11:33:47 +0200

    change version to 9.0.2

--- linux/include/linux/fusion.h
+++ linux/include/linux/fusion.h
@@ -22,7 +22,7 @@
 /* Fusion supports all API versions up to this version */
 #define FUSION_API_MAJOR_PROVIDED 9
 #define FUSION_API_MINOR_PROVIDED 0
-#define FUSION_API_MICRO_PROVIDED 1
+#define FUSION_API_MICRO_PROVIDED 2
 
 /*
  * Fusion Kernel Device API Version
